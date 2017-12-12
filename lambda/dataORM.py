327a72425253daaded59edd67d5fcb1cf7a83846
daniel_ahcardona <daniel_ahcardona>
2017-12-11 21:35:21 -0600

    FIX: Python ORM file, wrong file was versioend

--- lambda/dataORM.py
+++ lambda/dataORM.py
@@ -1,1 +1,36 @@
-class Risk:	"This is my second class"	a = 10		def __init__(self, code, riskName):                self.code = code        self.riskName = riskName		def func(self):		print('Hello')class FieldType:	"   "	fieldtTypeId	# Output: 10print(Risk.a)# Output: <function Risk.func at 0x0000000003079BF8>print(Risk.func)# Output: 'This is my second class'print(Risk.__doc__)
+class Risk:
+	"Risk"
+
+	def __init__(self, code, riskName):
+		"""Constructor"""
+		self.code = code
+		self.riskName = riskName
+
+class FieldType:
+	"FieldType, here should be configured the types: text, enum, number, etc"
+
+	def __init__(self, fieldTypeId, name):
+		"""Constructor"""
+		self.fieldTypeId = fieldTypeId
+		self.name = name
+
+class RiskField:
+	"RiskField"
+
+	def __init__(self, riskCode, riskFieldId, riskType):
+		"""Constructor"""
+        # This should be a reference of Risk
+		self.riskCode = riskCode
+		self.riskFieldId = riskFieldId
+        # This should be a reference of FieldType, it must be a list
+		self.riskType = riskType
+
+class RiskFieldOption:
+	"RiskFieldOption"
+
+	def __init__(self, riskFieldOptionId, riskFieldId, value):
+		"""Constructor"""
+		self.riskFieldOptionId = riskFieldOptionId
+        # This should be a reference of RiskField
+		self.riskFieldId = riskFieldId
+		self.value = value
